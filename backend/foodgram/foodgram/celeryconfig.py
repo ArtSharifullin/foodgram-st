f15cbc60a1065eecd5118696394a632886f30903
Timofey <108810276+ShabalinTK@users.noreply.github.com>
2025-11-18 11:50:55 +0300

    Add files via upload

--- backend/foodgram/foodgram/celeryconfig.py
+++ backend/foodgram/foodgram/celeryconfig.py
@@ -1,6 +1,11 @@
 import os
 
-broker_url = os.getenv('CELERY_BROKER_URL')
+rabbitmq_user = os.getenv('RABBITMQ_USER', 'testuser')
+rabbitmq_password = os.getenv('RABBITMQ_PASSWORD', 'testpass')
+rabbitmq_host = os.getenv('RABBITMQ_HOST', 'localhost')
+rabbitmq_port = os.getenv('RABBITMQ_PORT', '5672')
+
+broker_url = f'amqp://{rabbitmq_user}:{rabbitmq_password}@{rabbitmq_host}:{rabbitmq_port}//'
 result_backend = 'rpc://'
 task_serializer = 'json'
 result_serializer = 'json'
